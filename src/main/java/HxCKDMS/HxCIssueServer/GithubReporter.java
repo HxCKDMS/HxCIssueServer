4f0921f04f1180e0cff48e2ba897c3ddb0db8084
karelmikie3 <karelkling1@gmail.com>
2015-07-25 01:58:57 +0200

    server done.

---
+++ src/main/java/HxCKDMS/HxCIssueServer/GithubReporter.java
@@ -0,0 +1,77 @@
+package HxCKDMS.HxCIssueServer;
+
+import org.kohsuke.github.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+
+public class GithubReporter extends Thread {
+    private String crash;
+    private String mod;
+    private String title;
+    private String version;
+    String pasteeeLink;
+
+    public GithubReporter(String crash, String mod, String title, String version) {
+        this.crash = crash;
+        this.mod = mod;
+        this.title = title;
+        this.version = version;
+    }
+
+    @Override
+    public void run() {
+        try {
+            if(shouldUpload()) {
+                pasteeeLink = PasteeePoster.sendCrash(crash, mod, title);
+                if(pasteeeLink != null) github();
+            } else {
+                System.out.println("File didn't have to be uploaded!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void github() throws IOException{
+        GitHub github = GitHub.connectUsingOAuth(HxCIssueServer.githubAuthenticationKey);
+        GHRepository repository = github.getOrganization("HxCKDMS").getRepository("Version-and-Crashes");
+
+        List<GHIssue> openedIssues = repository.getIssues(GHIssueState.OPEN);
+
+        for(GHIssue issue : openedIssues){
+            if(issue.getTitle().equals(title)) {
+                issue.comment(pasteeeLink);
+                return;
+            }
+        }
+
+        GHIssueBuilder issueBuilder = repository.createIssue(title);
+
+        issueBuilder.label("bot");
+        issueBuilder.body(pasteeeLink);
+        issueBuilder.create();
+    }
+
+    private boolean shouldUpload() throws IOException {
+        URL url = new URL("https://raw.githubusercontent.com/HxCKDMS/Version-and-Crashes/master/Versions");
+        InputStream inputStream = url.openStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String line;
+        boolean answer = false;
+        while ((line = reader.readLine()) != null) {
+            if (line.startsWith(mod)) {
+                answer = Integer.parseInt(version.replace(".", "")) >= Integer.parseInt(line.replace(mod + ":", "").replace(".", ""));
+                break;
+            }
+        }
+
+        System.out.println(answer);
+        return answer;
+    }
+}
---
+++ src/main/java/HxCKDMS/HxCIssueServer/HxCIssueServer.java
@@ -0,0 +1,91 @@
+package HxCKDMS.HxCIssueServer;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HxCIssueServer {
+    private static boolean running = true;
+    private static ServerSocket serverSocket;
+    private static Socket connection;
+    private static BufferedReader reader;
+    private static Gson gson = new Gson();
+
+    public static String githubAuthenticationKey;
+    public static String pasteeeAuthenticationKey;
+
+    public static void main(String[] args) {
+        if(args.length != 3){
+            System.err.println("Program doesn't accept less and more than 3 arguments [port] [github key] [pasteee key]");
+        }
+        int port = Integer.parseInt(args[0]);
+        githubAuthenticationKey = args[1];
+        pasteeeAuthenticationKey = args[2];
+
+        try{
+            serverSocket = new ServerSocket(port, 100);
+        } catch (IOException e) {
+            System.err.println("Failed to bind to port: " + port + ".");
+            running = false;
+            System.exit(1);
+        }
+
+        while(running) {
+            try {
+                waitForConnection();
+                setupInputStream();
+                whileReceivingCrash();
+                closeConnections();
+            } catch (IOException e) {
+                System.err.print("connection lost.");
+            }
+        }
+    }
+
+    private static void waitForConnection() throws IOException{
+        System.out.println("waiting...");
+        connection = serverSocket.accept();
+        System.out.println("Connected to: " + connection.getInetAddress().getHostName());
+    }
+
+    private static void setupInputStream() throws IOException{
+        reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        System.out.println("setting up streams done");
+    }
+
+    private static void whileReceivingCrash() throws IOException{
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) sb.append(line).append("\n");
+
+        String crash = gson.fromJson(sb.toString(), crashSendTemplate.class).crash;
+        String mod = gson.fromJson(sb.toString(), crashSendTemplate.class).mod;
+        String title = gson.fromJson(sb.toString(), crashSendTemplate.class).title;
+        String version = gson.fromJson(sb.toString(), crashSendTemplate.class).version;
+
+        new GithubReporter(crash, mod, title, version).run();
+    }
+
+    private static void closeConnections() throws IOException{
+        reader.close();
+        connection.close();
+    }
+
+    class crashSendTemplate {
+        String crash;
+        String mod;
+        String title;
+        String version;
+
+        public crashSendTemplate(String crash, String mod, String title, String version) {
+            this.crash = crash;
+            this.mod = mod;
+            this.title = title;
+            this.version = version;
+        }
+    }
+}
